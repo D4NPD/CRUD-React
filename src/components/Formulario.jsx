dd22ebd7a777911fbed9edac46fbb2e00246722b
Daniel Pizarro <danypd1012@gmail.com>
2023-04-18 23:08:05 -0500

    La funcion editar ha sido creada e implementada

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -11,6 +11,8 @@ const Formulario = () => {
     const [telefono, setTelefono] = useState('')
     const [genero, setGenero] = useState('')
     const [carrera, setCarrera] = useState('')
+    const [registrado, setRegistrado] = useState(false)
+    const [id, setId] = useState('')
     const [listaEstudiantes, setListaEstudiantes] = useState([])
 
     useEffect(()=>{
@@ -64,7 +66,7 @@ const Formulario = () => {
         }   
     }
 
-    const eliminar = async (id) =>{
+    const eliminar = async id =>{
         try {
             await deleteDoc(doc(db,'estudiantes',id))
         } catch (error) {
@@ -72,12 +74,63 @@ const Formulario = () => {
         }
     }
 
+    const edicion = estudiante =>{
+        setNombre(estudiante.nombre)
+        setApellido(estudiante.apellido)
+        setCorreo(estudiante.correo)
+        setNacimiento(estudiante.nacimiento)
+        setTelefono(estudiante.telefono)
+        setGenero(estudiante.genero)
+        setCarrera(estudiante.carrera)
+        setId(estudiante.id)
+        setRegistrado(true)
+    }
+
+    const editar = async e =>{
+        e.preventDefault()
+        const docRef = doc(db,'estudiantes',id)
+        await updateDoc(docRef,{
+            nombre,
+            apellido,
+            correo,
+            nacimiento,
+            telefono,
+            genero, 
+            carrera
+        })
+
+        const nuevoArray = listaEstudiantes.map(
+            estudiante => estudiante.id === id ? {id, nombre, apellido, correo, nacimiento, telefono, genero, carrera} : estudiante
+        )
+        setListaEstudiantes(nuevoArray)
+        setNombre('')
+        setApellido('')
+        setCorreo('')
+        setNacimiento('')
+        setTelefono('')
+        setGenero('')
+        setCarrera('')
+        setId('')
+        setRegistrado(false)
+    }
+    
+    const cancelar = () =>{
+        setNombre('')
+        setApellido('')
+        setCorreo('')
+        setNacimiento('')
+        setTelefono('')
+        setGenero('')
+        setCarrera('')
+        setRegistrado(false)
+    }
+
     return(
         <div className="container mt-5">
         <div className="row">
           <div className="col-md-6 offset-md-3">
-            <h1>Registro de estudiante</h1>
-            <form onSubmit={registrar}>
+            <h1>{registrado ? 'Editar' : 'Registrar'} estudiante</h1>
+            <form onSubmit={registrado ? editar : registrar}>
               <div className="form-group mt-2">
                 <label htmlFor="nombre">Nombre:</label>
                 <input type="text" className="form-control" id="nombre" name="nombre" placeholder="Ingresa tu nombre" value={nombre} onChange={(e)=>setNombre(e.target.value)}/>
@@ -111,7 +164,16 @@ const Formulario = () => {
                 <label htmlFor="carrera">Carrera que estás estudiando:</label>
                 <input type="text" className="form-control" id="carrera" name="carrera" placeholder="Ingresa la carrera que estás estudiando" value={carrera} onChange={(e)=>setCarrera(e.target.value)}/>
               </div>
-              <button type="submit" className="btn btn-primary my-2">Enviar</button>
+              {
+                registrado ? 
+                (
+                    <>
+                        <button type="submit" className="btn btn-success my-2">Editar</button>  
+                        <button type="button"  className="btn btn-danger my-2" onClick={()=>cancelar()}>Cancelar</button>
+                    </>
+                ):
+                    <button type="submit" className="btn btn-primary my-2">Enviar</button>
+              }
             </form>
           </div>
         </div>
@@ -124,7 +186,7 @@ const Formulario = () => {
                         <div className='texto-solicitud'>
                           {estudiante.nombre} | {estudiante.apellido} | {estudiante.correo} | {estudiante.nacimiento} | {estudiante.telefono} | {estudiante.genero} | {estudiante.carrera}
                         </div>
-                        <div className='btn btn-outline-secondary'>Editar</div>
+                        <div className='btn btn-outline-secondary' onClick={()=>edicion(estudiante)}>Editar</div>
                         <div className='btn btn-danger' onClick={()=>eliminar(estudiante.id)}>Eliminar</div>
                     </div>
                 ))
